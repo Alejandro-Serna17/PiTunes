c896852fc0deaaaa9319c8d2a375a212983f9918
erejfir56 <162080315+erejfir56@users.noreply.github.com>
2024-12-24 14:52:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -45,23 +45,23 @@ while True:
                 print("Unknown URI format, check the tag's value.")
 
             # Basic Spotify controls, update with your own tag id's
-            if (id==481439137621):# Skip Forward
+            if (id==481439137621): # Skip Forward
                 sp.next_track(device_id=deviceId)
                 sp.start_playback(device_id=deviceId)
-            elif (id==894108319680):# Backtrack
+            elif (id==894108319680): # Backtrack
                 sp.previous_track(device_id=deviceId)
                 sp.previous_track(device_id=deviceId)
                 sp.start_playback(device_id=deviceId)
-            elif (id==825439240140):# Pause
+            elif (id==825439240140): # Pause
                 sp.pause_playback(device_id=deviceId)
                 continue # Make sure we don't reach the next start playback 
-            elif (id==894544592811):# Resume
+            elif (id==894544592811): # Resume
                 sp.start_playback(device_id=deviceId)
-            elif (id==276472086309):# Shuffle On
+            elif (id==276472086309): # Shuffle On
                 sp.shuffle(state=True, device_id=deviceId)
                 sp.start_playback(device_id=deviceId)
                 print("Shuffle On")
-            elif (id==207718989654):# Shuffle Off
+            elif (id==207718989654): # Shuffle Off
                 sp.shuffle(state=False, device_id=deviceId)
                 sp.start_playback(device_id=deviceId)
                 print("Shuffle Off")
