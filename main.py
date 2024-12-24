99ed77d9db8b62c8ccccab82732edd664e401111
erejfir56 <162080315+erejfir56@users.noreply.github.com>
2024-12-24 14:50:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 import re
 
+GPIO.setwarnings(False)
+
 deviceId=""
 clientId=""
 clientSecret=""
@@ -23,68 +25,49 @@ while True:
     try:
         reader = SimpleMFRC522()
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientId, client_secret=clientSecret, redirect_uri="http://localhost:8080", scope="user-read-playback-state,user-modify-playback-state"))
-
+        
         while True:
             print("\n\nAwaiting for scan...")
-            id = reader.read()[0] # Tag id
-            uri = reader.read()[1].strip() # Tag value(URI)
+            id=reader.read()[0]  # Reads the ID of the scanned tag
+            uri=reader.read()[1].strip()  # Read the URI written on the tag
             print("Tag Id:", id)
             print("Tag Value:", uri)
+                
             sp.transfer_playback(device_id=deviceId, force_play=False)
 
-            # Play songs, albums, playlists
-            if uri:
-                if isSong(uri):  # If it's a track
-                    sp.start_playback(device_id=deviceId, uris=[uri])  # Use uris for track
-                elif isAlbum(uri):  # If it's an album
-                    sp.start_playback(device_id=deviceId, context_uri=uri)  # Use context_uri for album
-                elif isPlaylist(uri):  # If it's a playlist
-                    sp.start_playback(device_id=deviceId, context_uri=uri)  # Use context_uri for playlist
-                elif uri.strip(): # If there is no value yet
-                    pass
-                else:
-                    print("Unknown URI format, check the tag's value.")
-            sleep(2)
+            if isSong(uri):  # If it's a track
+                sp.start_playback(device_id=deviceId, uris=[uri])  # Use uris for track
+            elif isAlbum(uri) or isPlaylist(uri):  # If it's an album or a playlist
+                sp.start_playback(device_id=deviceId, context_uri=uri)  # Use context_uri for album/playlist
+            elif uri.strip(): # If there is no value yet
+                pass
+            else:
+                print("Unknown URI format, check the tag's value.")
 
             # Basic Spotify controls, update with your own tag id's
             if (id==481439137621):# Skip Forward
                 sp.next_track(device_id=deviceId)
                 sp.start_playback(device_id=deviceId)
-                sleep(2)
             elif (id==894108319680):# Backtrack
                 sp.previous_track(device_id=deviceId)
                 sp.previous_track(device_id=deviceId)
                 sp.start_playback(device_id=deviceId)
-                sleep(2)
             elif (id==825439240140):# Pause
                 sp.pause_playback(device_id=deviceId)
-                #print("Music is paused")
-                sleep(2)
+                continue # Make sure we don't reach the next start playback 
             elif (id==894544592811):# Resume
                 sp.start_playback(device_id=deviceId)
-                #print("Music is playing")
-                sleep(2)
             elif (id==276472086309):# Shuffle On
                 sp.shuffle(state=True, device_id=deviceId)
                 sp.start_playback(device_id=deviceId)
                 print("Shuffle On")
-                sleep(2)
             elif (id==207718989654):# Shuffle Off
                 sp.shuffle(state=False, device_id=deviceId)
                 sp.start_playback(device_id=deviceId)
                 print("Shuffle Off")
-                sleep(2)
-
-            playbackState = sp.current_playback()  # Check if there is any playback information
-            if playbackState:
-                songName = playbackState['item']['name']
-                artist = playbackState['item']['artists'][0]['name']
-                albumName = playbackState['item']['album']['name']
-
-                print(f"Now Playing: {songName} - {albumName}")
-                print(f"By {artist}")
-            else:
-                print("No playback information available.")
+                
+            sleep(1.5)
+            sp.start_playback(device_id=deviceId) # Continue playing songs when in an album
 
     except Exception as e:
         print(e)
