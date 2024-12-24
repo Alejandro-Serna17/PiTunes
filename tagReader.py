4c915789926fbf3e94de265c91d8d917ecd95df2
erejfir56 <162080315+erejfir56@users.noreply.github.com>
2024-12-24 17:24:17 -0500

    Update tagReader.py

--- tagReader.py
+++ tagReader.py
@@ -8,14 +8,14 @@ GPIO.setwarnings(False)
 while True:
     reader = SimpleMFRC522()
     try:
-            print("\nAwaiting for scan...")
-            tagId = reader.read()[0] # Tag Id
-            tagValue = reader.read()[1] # Tag Value
-            print("RFID tag id:", tagId)
-            print("RFID tag value:", tagValue)
+        print("\nAwaiting for scan...")
+        tagId = reader.read()[0] # Tag Id
+        tagValue = reader.read()[1] # Tag Value
+        print("RFID tag id:", tagId)
+        print("RFID tag value:", tagValue)
     except Exception as e:
         print("\nError reading:", e)
     finally:
-            print("\nCleaning...")
-            GPIO.cleanup()
-            sleep(2)
+        print("\nCleaning...")
+        GPIO.cleanup()
+        sleep(2)
