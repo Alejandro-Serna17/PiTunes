b1cd6ebba252ed01dbba058473f759d8e5d74924
erejfir56 <162080315+erejfir56@users.noreply.github.com>
2024-12-24 14:40:15 -0500

    Update tagWriter.py

--- tagWriter.py
+++ tagWriter.py
@@ -3,10 +3,12 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from time import sleep
 
+GPIO.setwarnings(False)
+
 while True:
     reader = SimpleMFRC522()
     try:
-        uri = input("\nEnter the Spotify URI (ex. spotify:track:7KA4W4McWYRpgf0fWsJZWB): ")
+        uri = input("\nEnter the Spotify URI (e.g., spotify:track:7KA4W4McWYRpgf0fWsJZWB): ")
         print("Double check Spotify URI:", uri)
         print("Place RFID tag on reader now.")
         reader.write(uri)
@@ -14,7 +16,6 @@ while True:
     except Exception as e:
         print("Error writing to RFID tag:", e)
     finally:
-        print("Cleaning...")
+        print("\nCleaning...")
         GPIO.cleanup()
         sleep(2)
-
